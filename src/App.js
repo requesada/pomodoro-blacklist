a4d41eac73fb9a3e14d84a2ab23220143dc059ec
Roberto Quesada <rquesada@protonmail.ch>
2024-03-01 14:54:32 -0800

    Add a folder for components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import VolumeSlider from './components/VolumeSlider.js';
 
 const App = () => {
-  return <h1>Hi</h1>
+  return (
+    <>
+      <h1>Yo</h1>
+      <VolumeSlider />
+    </>
+  )
 };
 
 export default App
---
+++ src/components/VolumeSlider.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VolumeSlider = () => {
+    return (
+        <h1>hi</h1>
+    )
+}
+
+export default VolumeSlider
