7deab2d635c8241ce7c298303f05dc69dca8df36
Roberto Quesada <rquesada@protonmail.ch>
2024-03-07 14:25:12 -0800

    Start writing the template

--- popup/pomodoro_blacklist.js
+++ popup/pomodoro_blacklist.js
@@ -1,7 +1,21 @@
+const template = document.createElement('template')
+template.innerHTML = `
+    <style>
+        span {color: red}
+    </style>
+    <svg
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <slot></slot>
+    </svg>
+`
+
 class OliveCounter extends HTMLElement {
     constructor() {
         super()
-        this.innerHTML = "Hi, I'm custom"
+        const shadow = this.attachShadow({mode: 'open'})
+        shadow.append(template.content.cloneNode(true))
     }
 }
 
