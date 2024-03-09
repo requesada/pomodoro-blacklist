d5160370cca6c3d63414eebbcd27cae64c9220ea
Roberto Quesada <rquesada@protonmail.ch>
2024-03-09 11:40:09 -0800

    Add basic olive component

--- popup/pomodoro_blacklist.js
+++ popup/pomodoro_blacklist.js
@@ -1,21 +1,20 @@
-const template = document.createElement('template')
-template.innerHTML = `
-    <style>
-        span {color: red}
-    </style>
-    <svg
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-    >
-        <slot></slot>
-    </svg>
-`
-
 class OliveCounter extends HTMLElement {
     constructor() {
         super()
-        const shadow = this.attachShadow({mode: 'open'})
-        shadow.append(template.content.cloneNode(true))
+        const shadowRoot = this.attachShadow({mode: 'open'})
+        const template = document.createElement('template')
+        template.innerHTML = `
+            <svg
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24px"
+                height="24px"
+            >
+                <circle cx="12" cy="12" r="11px" stroke="#001011" stroke-width="1px" fill="#92DE91" />
+                <circle cx="12" cy="12" r="4.5px" stroke="#001011" stroke-width="1px" fill="#F95738" />
+            </svg>
+        `
+        shadowRoot.append(template.content.cloneNode(true))
     }
 }
 
