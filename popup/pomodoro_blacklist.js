81b01aff003ad349396aae3248e18346ed0a2408
Roberto Quesada <rquesada@protonmail.ch>
2024-03-16 11:21:39 -0700

    Implement simple start/stop behavior

--- popup/pomodoro_blacklist.js
+++ popup/pomodoro_blacklist.js
@@ -1,58 +1,49 @@
 let round = 0
 
 let phaseIndex = 0
-const phase = ['ready', 'counting', 'done']
+const phase = ['work counting', 'work done', 'break counting', 'break done']
 
-let directiveIndex = 0
-const directive = ['work', 'break']
-
-const incrementRound = () => {
-  if (round < 3) {
-    round++
-  } else {
-    round = 0
-  }
+let intervalID
+let startingMinutes = 25
+const resetTimer = (newMinutes) => {
+  clearInterval(intervalID)
+  document.querySelector('#display').innerHTML = `${String(newMinutes).padStart(2, '0')}:00`
 }
 
-const incrementPhase = () => {
-  if (phaseIndex < phase.length - 1) {
-    phaseIndex++
-  } else {
-    phaseIndex = 0
-  }
+const stopTimer = () => {
+  phaseIndex = 0
+  resetTimer()
 }
 
-const incrementDirective = () => {
-  directiveIndex = directiveIndex === 0 ? 1 : 0
-}
+const timer = () => {
+  let minutes = startingMinutes - 1
+  let seconds = 59
 
-const timer = (startingMinutes, setTime) => {
-  if (setTime) {
-    document.querySelector('#display').innerHTML = `${String(startingMinutes).padStart(2, '0')}:00`
-  } else {
-    let minutes = startingMinutes - 1
-    let seconds = 59
-    let intervalID
-
-    const subtractSecond = () => {
-      document.querySelector('#display').innerHTML = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
-      if (seconds === 0 && minutes > 0) {
-        minutes--
-        seconds = 59
-      } else if (seconds > 0) {
-        seconds--
-      } else {
-        clearInterval(intervalID)
-      }
+  const subtractSecond = () => {
+    document.querySelector('#display').innerHTML = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
+    if (seconds === 0 && minutes > 0) {
+      minutes--
+      seconds = 59
+    } else if (seconds > 0) {
+      seconds--
+    } else {
+      clearInterval(intervalID)
     }
+  }
 
-    intervalID = setInterval(subtractSecond, 1000)
+  intervalID = setInterval(subtractSecond, 1000)
+}
+
+const timerButton = document.querySelector('#timer-button')
+const clickTimerButton = () => {
+  if (timerButton.className === 'start-button') {
+    timerButton.className = 'stop-button'
+    timerButton.innerHTML = 'Stop'
+    timer(25)
+  } else {
+    timerButton.className = 'start-button'
+    timerButton.innerHTML = 'Start'
+    resetTimer(25)
   }
 }
-
-const clickStartButton = () => {
-  const startButton = document.querySelector('#start-button')
-  startButton.id = 'stop-button'
-  startButton.innerHTML = 'Stop'
-  timer(25)
-}
+timerButton.addEventListener('click', clickTimerButton)
