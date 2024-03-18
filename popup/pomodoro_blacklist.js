19b4581a6b82f18ab5038d61525bc0fb1c4cc7d3
Roberto Quesada <rquesada@protonmail.ch>
2024-03-18 13:45:31 -0700

    Continuing to work through the logic

--- popup/pomodoro_blacklist.js
+++ popup/pomodoro_blacklist.js
@@ -3,6 +3,8 @@ let round = 0
 let phaseIndex = 0
 const phase = ['work-counting', 'work-done', 'break-counting', 'break-done']
 
+const currentRound = document.querySelector(`#round-${round}`)
+
 let intervalID
 let startingMinutes = 25
 const resetTimer = (newMinutes) => {
@@ -15,14 +17,29 @@ const stopTimer = () => {
   resetTimer(25)
 }
 
-const getCurrentPlace = () => {
-  return document.querySelector(`#round-${round}`).className
+const timerButton = document.querySelector('#timer-button')
+const clickTimerButton = () => {
+  if (timerButton.className === 'start-button') {
+    timerButton.className = 'stop-button'
+    timerButton.innerHTML = 'Stop'
+    if (currentRound.className !== 'ready' && phaseIndex < phase.length - 1) {
+      phaseIndex++
+    }
+    currentRound.className = phase[phaseIndex]
+    timer(25)
+  } else {
+    timerButton.className = 'start-button'
+    timerButton.innerHTML = 'Start'
+    phaseIndex = 0
+    resetTimer(25)
+  }
 }
-
+timerButton.addEventListener('click', clickTimerButton)
 
 const timer = () => {
-  let minutes = startingMinutes - 1
-  let seconds = 59
+  // let minutes = startingMinutes - 1
+  let minutes = 0
+  let seconds = 2
 
   const subtractSecond = () => {
     document.querySelector('#display').innerHTML = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
@@ -36,6 +53,7 @@ const timer = () => {
       if (round === 3 && phaseIndex === phase.length - 1) {
         round = 0
         phaseIndex = 0
+        document.querySelectorAll('div[id^="round-"]').forEach((node) => {node.className = 'ready'})
         resetTimer(25)
       } else if (phaseIndex === phase.length - 1) {
         round++
@@ -43,23 +61,13 @@ const timer = () => {
         resetTimer(25)
       } else {
         phaseIndex++
+        document.querySelector(`#round-${round}`).className = phase[phaseIndex]
+        timerButton.className = 'start-button'
+        timerButton.innerHTML = 'Start'
       }
     }
+    console.log({round, phaseIndex})
   }
 
   intervalID = setInterval(subtractSecond, 1000)
 }
-
-const timerButton = document.querySelector('#timer-button')
-const clickTimerButton = () => {
-  if (timerButton.className === 'start-button') {
-    timerButton.className = 'stop-button'
-    timerButton.innerHTML = 'Stop'
-    timer(25)
-  } else {
-    timerButton.className = 'start-button'
-    timerButton.innerHTML = 'Start'
-    resetTimer(25)
-  }
-}
-timerButton.addEventListener('click', clickTimerButton)
